8778c4113f58670486d93e73d6e9e21bef9a66c2
Mike Minckler <mikeminckler@gmail.com>
2018-05-27 19:48:56 -0700

    adding sites to web section

--- resources/assets/js/routes/WebDevelopment.vue
+++ resources/assets/js/routes/WebDevelopment.vue
@@ -4,7 +4,8 @@
 
         <div class="header">Experience</div>
         <div class="content-item">
-            <p>Since 2005 a professional web developer and content creator in the areas of  web design, video production, photography, graphic design, and 3D animation. An adaptive life-long learner always improving and staying current with the latest in progressive technology and digital media.</p>
+
+            <p>In the departments of Information Technology, and subsuquently Marketing, I was responsible </p>
 
             <grid-list :items="['Laravel', 'VueJS', 'PHP', 'HTML', 'CSS' ]"></grid-list>
 
@@ -12,18 +13,47 @@
 
         <div class="header">Projects</div>
 
-        <div class="content-item">
-            <div class="sub-header">BlueHealth Inc</div>
-            <div class="dates">September 2014 - Present</div>
-            <p>Electronic Medical charting and management software.</p>
-            <grid-list width="150px" :items="['Laravel + VueJS', '1100+ patients', 'Encrypted DB', '2FA', 'Form Building', 'Reporting', 'Invoicing', 'Inventory', 'Medication Dispensing']"></grid-list>
-        </div>
+        <div class="content-item websites">
 
-        <div class="content-item">
-            <div class="sub-header">Brentwood College School</div>
-            <div class="dates">September 2014 - Present</div>
-            <p>Electronic Medical charting and management software.</p>
-            <grid-list width="150px" :items="['Laravel + VueJS', '1100+ patients', 'Encrypted DB', '2FA', 'Form Building', 'Reporting', 'Invoicing', 'Inventory', 'Medication Dispensing']"></grid-list>
+            <div class="website">
+
+                <div class="thumbnail">
+                    <img src="/images/web/bcs.jpg" class="thumbnail" />
+                </div>
+
+                <div class="website-content">
+                    <div class="sub-header">BlueHealth Inc</div>
+                    <div class="dates">September 2014 - Present</div>
+                    <div class="description">Medical charting and management application</div>
+                    <div class="role">Co-Founder, Full Stack Programmer</div> 
+                    <grid-list width="150px" :items="['Laravel + VueJS', '1100+ patients', 'Encrypted DB', '2FA', 'Form Building', 'Reporting', 'Invoicing', 'Inventory', 'Medication Dispensing']"></grid-list>
+
+                </div>
+
+            </div>
+
+
+            <div v-for="(site, index) in sites"
+                class="website"
+            >
+
+                <div class="thumbnail">
+                    <img src="/images/web/bcs.jpg" class="thumbnail" />
+                </div>
+                <div class="website-content">
+                    <div class="sub-header">Brentwood College School</div>
+                    <div class="dates">September 2005 - Present</div>
+                    <div class="url"><a href="https://www.brentwood.bc.ca" target="_blank">www.brentwood.bc.ca</a></div>
+                    <div class="role">Lead developer for Brentwood College School promotional website.</div>
+                    <grid-list width="150px" :items="['PHP + JS', 'Typo3 CMS', 'CMS Custom Templates', 'Responsive Design', 'YouTube Integration']"></grid-list>
+                    <div class="more-info" @click="showMore(website)">
+                        <div class="more-info-link">More info</div>
+                        <i class="fas fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
 
         <div class="header">Side Projects</div>
@@ -31,6 +61,21 @@
         
         </div>
 
+
+        <transition name="fade">
+            <div class="image-viewer" 
+                id="image-viewer" 
+                v-if="selectedSite"
+            >
+                <div class="image-viewer-background" @click="deselectSite()"></div>
+                <div class="image-advance-left" @click="prevSite()"><div class="fas fa-chevron-left"></div></div>
+                <div class="image-advance-right" @click="nextSite()"><div class="fas fa-chevron-right"></div></div>
+                <div class="image-viewer-close" @click="deselectSite()"><div class="fas fa-times-circle"></div></div>
+                <transition name="fade" mode="out-in">
+                    <img v-if="selectedSite" :src="'/images/' + selectedSite" :key="selectedSite">
+                </transition>
+            </div>
+        </transition>
     </div>
 
 </template>
@@ -41,17 +86,16 @@
 
         data: function() {
             return {
-                contributions: ''
+                selectedSite: '',
+                sites: [
+                    {
+                    
+                    },
+                ]
             }
         },
 
         mounted() {
-
-            this.$http.get('https://github.com/users/mikeminckler/contributions').then( response => {
-                this.contributions = response.data;
-            }, error => {
-            
-            });
 
         }
 

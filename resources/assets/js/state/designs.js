1f412bb745d1ed747984e7fe7f9658051bf6d3d9
Mike Minckler <mikeminckler@gmail.com>
2018-07-07 12:27:37 -0700

    photo updates

--- resources/assets/js/state/designs.js
+++ resources/assets/js/state/designs.js
@@ -1,18 +1,18 @@
 const designs = [
-    'rowing_boys.jpg',
     'rowing_girls.jpg',
-    'rugby_girls.jpg',
     'soccer.jpg',
-    'campus_map.jpg',
     'eod13.jpg',
     'phantom.jpg',
+    'campus_map.jpg',
+    'cfawe13.jpg',
     'les_miz.jpg',
     'plaza.jpg',
-    'cfawe13.jpg',
     'admissions_pop_up.jpg',
     'bcs_dance.jpg',
     'cfawe12.jpg',
     'home_banner.jpg',
+    'rowing_boys.jpg',
+    'rugby_girls.jpg',
 ];
 
 export default designs;
